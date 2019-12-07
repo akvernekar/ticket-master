076c74be543fc2f76d519f3709bc6b856f111529
akshay vernekar <akvernekar@gmail.com>
2019-12-07 17:36:53 +0530

    configured baseurl

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios=Axios.create({
-    baseURL:'https://ticket-master-akshay123.herokuapp.com'
+    baseURL:'https://ticket-master-akshay123.herokuapp.com/api'
 })
 
 export default axios
