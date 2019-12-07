84ad4d380dab91751d872ce8059fc06843d8c163
akshay vernekar <akvernekar@gmail.com>
2019-12-07 13:56:56 +0530

    Added mongo atlas

--- config/database.js
+++ config/database.js
@@ -1,12 +1,15 @@
 const mongoose =require ('mongoose')
 
+console.log(process.env.MONGODB_URI)
 const connectDb=()=>{
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/july-ticket-master',{ useNewUrlParser: true, useUnifiedTopology: true  })
+mongoose.connect( process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  })
             .then(()=>{
-                console.log('connected to db')
+                console.log('connected to db: ' + process.env.MONGODB_URI)
             })
             .catch((err)=>{
                 console.log(err)
+                console.log('error to db' + process.env.MONGODB_URI)
+
             })}
 
 module.exports=connectDb          
--- index.js
+++ index.js
@@ -14,13 +14,11 @@ connectDb()
 app.use(cors())
 app.use('/',router)
 
-if (process.env.NODE_ENV === 'production'){
-     app.use(express.static( 'client/build' ));
+app.use(express.static( 'client/build' ));
 
-     app.get('*',(req,res)=>{
-          res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
-     });
-}
+app.get('*',(req,res)=>{
+     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port,()=>{
      console.log('listning to port', port)
